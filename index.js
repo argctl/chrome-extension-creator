10088f0998837098211760a467627991e4b2f819
David Kamer <david.kamer@uniwrighte.com>
2019-05-20 13:56:56 -0400

    fixed manifest from default

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const os = require('os')
 const path = require('path')
 const args = process.argv.slice(2)
 const packageJSON = require('./static/package.json')
+const manifestJSON = require('./static/manifest.json')
 const version = Number(process.version.match(/^v(\d+\.\d+)/)[1])
 
 function write (s) {
@@ -11,7 +12,7 @@ function write (s) {
 }
 
 function getStaticFileNames (staticPath) {
-  return fs.readdirSync(staticPath).filter(file => file !== 'package.json')
+  return fs.readdirSync(staticPath).filter(file => file !== 'package.json' && file !== 'manifest.json')
 }
 function copyContents (originPath, destinationPath, files) {
   files.forEach(file => {
@@ -36,10 +37,12 @@ function main () {
   const name = args[0]
   const author = os.userInfo().username
   const namedPackageFile = { ...packageJSON, name, author }
+  const namedManifestFile = { ...manifestJSON, name }
   fs.mkdirSync(name)
   const projectPath = path.join(process.cwd(), name)
   const staticPath = path.join(__dirname, 'static')
   fs.writeFileSync(path.join(projectPath, `package.json`), JSON.stringify(namedPackageFile, null, 2))
+  fs.writeFileSync(path.join(projectPath, `manifest.json`), JSON.stringify(namedManifestFile, null, 2))
   const files = getStaticFileNames(staticPath)
   copyContents(staticPath, projectPath, files)
   write(`DONE!
